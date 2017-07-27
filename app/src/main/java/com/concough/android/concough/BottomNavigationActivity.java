71a7e761075e4afc2d02ff939e544e9c6786fb86
abolfazl behroozi <abolfazl.beh@gmail.com>
2017-07-27 15:46:15 +0430

    Bottom Navigation Testing (abolfazl.beh)
    1 - Create BottomNavigationActivity and setup it to have bottom navigation
    2 - Every activity that needs to include bottom navigation must inherit from it.
    3 - Four icons created from image for bottom navigation

---
+++ app/src/main/java/com/concough/android/concough/BottomNavigationActivity.java
@@ -0,0 +1,76 @@
+package com.concough.android.concough;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.concough.android.singletons.FontCacheSingleton;
+import com.concough.android.utils.CustomTypefaceSpan;
+
+public class BottomNavigationActivity extends AppCompatActivity {
+
+
+    private int selectedItemId;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            selectedItemId = item.getItemId();
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_archive:
+                    return true;
+                case R.id.navigation_favorites:
+                    return true;
+                case R.id.navigation_settings:
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @LayoutRes
+    protected int getLayoutResourceId() {
+        return R.layout.activity_bottom_navigation;
+    }
+
+    protected void setMenuSelectedIndex(int index) {
+        switch (index) {
+            case 1: this.selectedItemId = R.id.navigation_archive; break;
+            case 2: this.selectedItemId = R.id.navigation_favorites; break;
+            case 3: this.selectedItemId = R.id.navigation_settings; break;
+            default: this.selectedItemId = R.id.navigation_home; break;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResourceId());
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        CustomTypefaceSpan typefaceSpan = new CustomTypefaceSpan("", FontCacheSingleton.getInstance(getApplicationContext()).getRegular());
+        for (int i =0; i < navigation.getMenu().size(); i++) {
+            MenuItem m = navigation.getMenu().getItem(i);
+            SpannableStringBuilder title = new SpannableStringBuilder(m.getTitle());
+            title.setSpan(typefaceSpan, 0, title.length(), 0);
+            m.setTitle(title);
+        }
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.getMenu().findItem(this.selectedItemId).setChecked(true);
+
+    }
+
+}
