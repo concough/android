18b12c75f6deb68722f04dd8e0cd3e2df22603f9
Abolfazl Behroozi <abolfazl.beh@gmail.com>
2018-10-11 00:02:55 +0330

    Models and Model Handlers (abolfazl.beh) - 2018-10-11
    1 - All model handlers that need username fixed and checked
    2 - Also some methods ("getAllLessons" and "getOneLessonByTitleAndOrder") added to EntranceLessonModelHandler

--- app/src/main/java/com/concough/android/models/EntranceLessonModelHandler.kt
+++ app/src/main/java/com/concough/android/models/EntranceLessonModelHandler.kt
@@ -4,13 +4,14 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
 import com.concough.android.singletons.RealmSingleton
+import io.realm.RealmResults
 
 /**
  * Created by abolfazl on 7/12/17.
  */
 class EntranceLessonModelHandler {
     companion object Factory {
-        val TAG: String = "EntranceLessonModelHandler"
+        const val TAG: String = "EntranceLessonModelHandler"
 
         @SuppressLint("LongLogTag")
         @JvmStatic
@@ -34,8 +35,49 @@ class EntranceLessonModelHandler {
 //                RealmSingleton.getInstance(context).DefaultRealm.commitTransaction()
                 return lesson
             } catch (exc: Exception) {
-                Log.d(TAG, exc.message)
+//                Log.d(TAG, exc.message)
 //                RealmSingleton.getInstance(context).DefaultRealm.cancelTransaction()
+            }
+
+            return null
+        }
+
+        @JvmStatic
+        fun getAllLessons(context: Context, username: String): ArrayList<EntranceLessonModel> {
+            var items = ArrayList<EntranceLessonModel>()
+
+            val entrances = RealmSingleton.getInstance(context).DefaultRealm.where(EntranceModel::class.java)
+                    .equalTo("username", username).findAll()
+
+            if (entrances.count() > 0) {
+                for (ent in entrances) {
+                    for (booklet in ent.booklets) {
+                        for (lesson in booklet.lessons) {
+                            items.add(lesson)
+                        }
+                    }
+                }
+            }
+            return items
+        }
+
+        @JvmStatic
+        fun getOneLessonByTitleAndOrder(context: Context, username: String, entranceUniqueId: String,
+                                        lessonTitle: String, lessonOrder: Int): EntranceLessonModel? {
+
+            val entrance = RealmSingleton.getInstance(context).DefaultRealm.where(EntranceModel::class.java)
+                    .equalTo("username", username)
+                    .equalTo("uniqueId", entranceUniqueId)
+                    .findFirst()
+
+            entrance?.let {
+                for (booklet in it.booklets) {
+                    for (lesson in booklet.lessons) {
+                        if (lesson.fullTitle == lessonTitle && lesson.order == lessonOrder) {
+                            return lesson
+                        }
+                    }
+                }
             }
 
             return null
